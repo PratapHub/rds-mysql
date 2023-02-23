c37979c99b7b97906f3f1b1603b15452e3dac4f1
PratapHub <74722681+PratapHub@users.noreply.github.com>
2023-02-23 13:51:43 +0530

    added vars
    added vars

--- modules/rds-mysql/variable.tf
+++ modules/rds-mysql/variable.tf
@@ -1,79 +1,29 @@
 variable "name" {
-  description = "The name of the RDS MySQL instance"
   type        = string
+  description = "Name of the RDS instance"
 }
 
 variable "username" {
-  description = "The username for the RDS MySQL instance"
   type        = string
+  description = "Username for the RDS instance"
 }
 
 variable "password" {
-  description = "The password for the RDS MySQL instance"
   type        = string
+  description = "Password for the RDS instance"
 }
 
 variable "availability_zone" {
-  description = "The availability zone for the primary RDS MySQL instance"
   type        = string
+  description = "The availability zone in which to create the primary RDS instance"
 }
 
 variable "secondary_availability_zones" {
-  description = "The list of availability zones for the secondary RDS MySQL instances"
   type        = list(string)
-  default     = []
+  description = "List of availability zones in which to create secondary RDS instances"
 }
 
 variable "subnet_ids" {
-  description = "The list of subnet IDs where RDS MySQL instance should be launched"
   type        = list(string)
+  description = "List of subnet IDs to launch RDS instances in"
 }
-
-variable "allocated_storage" {
-  description = "The amount of storage to allocate for the RDS MySQL instance (in gigabytes)"
-  type        = number
-  default     = 20
-}
-
-variable "engine" {
-  description = "The database engine to use for the RDS MySQL instance"
-  type        = string
-  default     = "mysql"
-}
-
-variable "engine_version" {
-  description = "The version of the database engine to use for the RDS MySQL instance"
-  type        = string
-  default     = "5.7"
-}
-
-variable "instance_class" {
-  description = "The instance class to use for the RDS MySQL instance"
-  type        = string
-  default     = "db.t2.micro"
-}
-
-variable "multi_az" {
-  description = "Specifies if the RDS MySQL instance is a Multi-AZ deployment"
-  type        = bool
-  default     = true
-}
-
-variable "backup_retention_period" {
-  description = "The number of days to retain backups for the RDS MySQL instance"
-  type        = number
-  default     = 7
-}
-
-variable "maintenance_window" {
-  description = "The maintenance window for the RDS MySQL instance"
-  type        = string
-  default     = "Mon:00:00-Mon:03:00"
-}
-
-variable "tags" {
-  description = "A map of tags to assign to the RDS MySQL instance"
-  type        = map(string)
-  default     = {}
-}
-
