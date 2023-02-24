51920ddae60155363ad1065197d700cddfb8c522
PratapHub <74722681+PratapHub@users.noreply.github.com>
2023-02-24 11:54:25 +0530

    removed dups

--- modules/rds-mysql/versions.tf
+++ modules/rds-mysql/versions.tf
@@ -9,34 +9,3 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_db_instance" "primary" {
-  # Create a MySQL database instance in the primary region
-}
-
-resource "aws_db_instance" "secondary" {
-  # Create a MySQL database instance in the secondary region
-}
-
-resource "aws_db_instance_replica" "secondary" {
-  # Create a read replica for the primary MySQL instance in the secondary region
-}
-
-resource "aws_security_group" "mysql" {
-  # Create a security group for the MySQL database instances
-}
-
-resource "aws_db_subnet_group" "mysql" {
-  # Create a subnet group for the MySQL database instances
-}
-
-resource "aws_db_parameter_group" "mysql" {
-  # Create a parameter group for the MySQL database instances
-}
-
-output "primary_endpoint" {
-  # Output the endpoint of the primary MySQL database instance
-}
-
-output "secondary_endpoints" {
-  # Output the endpoints of the read replicas in the secondary region
-}
