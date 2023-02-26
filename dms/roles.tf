bb50039f4c7b285ec44059d2abd7b5e4eee3277f
PratapHub <74722681+PratapHub@users.noreply.github.com>
2023-02-26 23:05:08 +0530

    roles.tf
    roles.tf

---
+++ dms/roles.tf
@@ -0,0 +1,49 @@
+resource "aws_iam_role" "dms" {
+  name = "dms-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "dms.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "dms" {
+  name = "dms-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "dms" {
+  policy_arn = aws_iam_policy.dms.arn
+  role       = aws_iam_role.dms.name
+}
